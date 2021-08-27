f9df2071f034d5e52b0d4f02f74a0e7ca8150b95
Kenneth Leung <kennethleung.ty@gmail.com>
2021-08-28 01:48:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import numpy as np
 import networkx as nx
 from pyvis.network import Network
 
-# Read data
-df_interactions = pd.read_csv('data/processed_drug_interactions.csv')
+# Read dataset (CSV)
+df_interact = pd.read_csv('data/processed_drug_interactions.csv')
 
 # Set header title
 st.title('Network Graph Visualization of Drug-Drug Interactions')
@@ -16,16 +16,17 @@ drug_list = ['Metformin', 'Glipizide', 'Lisinopril', 'Simvastatin',
             'Warfarin', 'Aspirin', 'Losartan', 'Ibuprofen']
 drug_list.sort()
 
-# Implement multiselect options for users (returns a list)
+# Implement multiselect dropdown menu for option selection (returns a list)
 selected_drugs = st.multiselect('Select drug(s) to visualize', drug_list)
 
-# Set display for initial site load
+# Set info message on initial site load
 if len(selected_drugs) == 0:
-    st.text('Please choose at least 1 drug')
+    st.text('Please choose at least 1 drug to get started')
 
 # Create network graph when user selects >= 1 item
 else:
-    df_select = df_interactions.loc[df_interactions['drug_1_name'].isin(selected_drugs) | df_interactions['drug_2_name'].isin(selected_drugs)]
+    df_select = df_interact.loc[df_interact['drug_1_name'].isin(selected_drugs) | \
+                                df_interact['drug_2_name'].isin(selected_drugs)]
     df_select = df_select.reset_index(drop=True)
 
     # Create networkx graph object from pandas dataframe
@@ -42,7 +43,7 @@ else:
     # Take Networkx graph and translate it to a PyVis graph format
     drug_net.from_nx(G)
 
-    # Generate network with specific layout
+    # Generate network with specific layout settings
     drug_net.repulsion(
                         node_distance=420,
                         central_gravity=0.33,
@@ -51,29 +52,27 @@ else:
                         damping=0.95
                        )
 
-    # Save and load graph as HTML file (on Streamlit Sharing)
+    # Save and read graph as HTML file (on Streamlit Sharing)
     try:
         path = '/tmp'
         drug_net.save_graph(f'{path}/pyvis_graph.html')
         HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-    # Save and load graph as HTML file (locally)
+    # Save and read graph as HTML file (locally)
     except:
         path = '/html_files'
         drug_net.save_graph(f'{path}/pyvis_graph.html')
         HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
 
-    # Read and load HTML file for display on Streamlit page
-    source_code = HtmlFile.read()
-    components.html(source_code, height=500, width=700)
+    # Load HTML file in HTML component for display on Streamlit page
+    components.html(HtmlFile.read(), height=500, width=700)
 
 # Footer
 st.markdown(
     """
     <br>
-    <br>
     <h6><a href="https://github.com/kennethleungty/Pyvis-Network-Graph-Streamlit" target="_blank">Project GitHub repo</a></h6>
     <h6><a href="https://kennethleungty.medium.com" target="_blank">Medium article</a></h6>
     <h6><a href="https://github.com/kennethleungty" target="_blank">Created by Kenneth Leung</a></h6>
-    """, unsafe_allow_html=True,
+    """, unsafe_allow_html=True
     )
